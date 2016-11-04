e749ee8aa4f0e8c42deedcfc0fc8176aa60f2d1b
Billy Jacobson <billy.jacobson@gmail.com>
2016-11-04 18:41:50 -0500

    simplifying initial version

--- intiial/hackathon-module.js
+++ intiial/hackathon-module.js
@@ -1,23 +1,15 @@
-// Declaration of the application and its requirements.
 var hackathon = angular.module('hackathon', []);
 
-/*
- * Creates an HTML element that we can use like this:
- *     <hackathon-list></hackathon-list>
- * 
- * This element fetches the hackathons from the server, displays them
- * and allows the user to navigate to the details page for specific hackathons.
- */
 hackathon.component('hackathonList', {
     templateUrl: 'templates/hackathon-list.htm',
-    controller: function HackathonListController($http, $location) {
-        this.hackathonList = [];
-
-        $http({
-            method: 'GET',
-            url: '/api/hackathons'
-        }).then(response => this.hackathonList = response.data.hackathons)
-            .catch(err => console.log('there was an error!', err));
+    controller: function HackathonListController($location) {
+        this.hackathonList = [{
+        'name':'ArchHacks', 
+        'id': 'archhacks', 
+        'logoUrl': 'http://archhacks.io/static/images/archhackslogo-title.png',
+        'rating': 5,
+        'region': 'Midwest'
+        }];
 
         this.viewHackathonDetails = function(hackathon) {
             $location.url(`/${hackathon.id}`);
